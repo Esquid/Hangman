fa2221587793d47e995026d583b5a53d4af82003
Jared E Garcia <43124120+Esquid@users.noreply.github.com>
2019-02-22 10:21:10 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -3,29 +3,27 @@ import csv
 
 
 def hang():
-    gallows = [',--;-', '| ', '| ', '| ', '| ', '| ', '|_____']
-    parts = iter([' 0 ', '/', '|', '\\', ' | ', ' A ', '/ ', '\\'])
-    sequence = [1, 3, 1, 1, 2]
-    for i, v in enumerate(sequence, start=1):
-        for k in range(v):
-            gallows[i] += next(parts)
-            yield '\n'.join(gallows)
-    raise StopIteration
+ gallows = [',--;-', '| ', '| ', '| ', '| ', '| ', '|_____']
+ parts = iter([' 0 ', '/', '|', '\\', ' | ', ' A ', '/ ', '\\'])
+ sequence = [1, 3, 1, 1, 2]
+ for i, v in enumerate(sequence, start=1):
+    for k in range(v):
+         gallows[i] += next(parts)
+         yield '\n'.join(gallows)
+ raise StopIteration
+  
 
 def game():
-  csv_file = open('words.csv')
-  secret = random.choice(list(csv_file))
-  blanks = "_ ".join("_ " for l in secret)
-  print(blanks)
-  guess = input("Guess a letter: ")
-  if guess != secret:
-    print(guess)
-    print(next(hang()))
-    input("Guess Again:")
-  if guess == secret:
-      print(guess for l in blanks)
-      print(blanks)
-      input("Guess Again:")
+ csv_file = open('words.csv')
+ secret = random.choice(list(csv_file))
+ blanks = "_ ".join("_ " for l in secret)
+ print(blanks)
+ guess = input("Guess a letter: ")
+ results = ('guess'.join(guess for l in blanks))
+ if guess != secret:
+    print(guess)and input("Guess Again:")
+ if guess == secret: 
+    print(results) and input("Guess Again:")
   
 
   
