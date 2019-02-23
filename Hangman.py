475c9f74a124ab3c6c446f0cbbfadd38ee84335a
Jared E Garcia <43124120+Esquid@users.noreply.github.com>
2019-02-22 23:47:20 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -8,22 +8,30 @@ def hang():
  sequence = [1, 3, 1, 1, 2]
  for i, v in enumerate(sequence, start=1):
     for k in range(v):
-         gallows[i] += next(parts)
-         yield '\n'.join(gallows)
+        gallows[i] += next(parts)
+        yield '\n'.join(gallows)
  raise StopIteration
   
 
 def game():
- csv_file = open('words.csv')
- secret = random.choice(list(csv_file))
- blanks = "_ ".join("_ " for l in secret)
- print(blanks)
- guess = input("Guess a letter: ")
- results = ('guess'.join(guess for l in blanks))
- if guess != secret:
-    print(guess)and input("Guess Again:")
- if guess == secret: 
-    print(results) and input("Guess Again:")
+  a = hang()
+  with open('words.csv') as f:
+   words = list(f)
+   word_list = [word[:-1] for word in words if word == word.lower() and len(word) > 4]
+   secret = random.choice(word_list)
+   progress = ['_' for letter in secret] 
+   print(progress)
+   guess = input("Guess a letter: ")
+   if guess in secret:
+    for idx, letter in enumerate(secret):
+     if letter == guess:
+       progress[idx] = guess
+       print(progress) 
+       input("Guess a letter: ")
+   while guess not in secret:
+      print(next(a))
+      print(progress)
+      input('Guess a letter: ')
   
 
   
